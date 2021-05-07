7646adaae73bfe54c54fb196d0354e878dc65e17
Count Zero <joelecox1@gmail.com>
2021-05-07 13:03:35 -0500

    basic file layout and html wireframe

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Project from './components/Project';
+import Footer from './components/Footer';
+
+function App() {
+  return(
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Project></Project>
+      <Footer></Footer>
+    </div>
+
+  )
+};
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return(
+    <footer>
+      <ul>
+        <li>Github</li>
+        <li>LinkedIn</li>
+        <li>Instagram</li>
+      </ul>
+    </footer>
+  )
+
+};
+
+export default Footer;
