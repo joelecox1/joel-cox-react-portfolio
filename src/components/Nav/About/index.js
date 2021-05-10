4449373a76dce74a26ed4a37868f92bb2c1575d0
Count Zero <joelecox1@gmail.com>
2021-05-10 16:24:46 -0500

    about component

---
+++ src/components/Nav/About/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className='about'>
+      <h2 className='section-header'>ABOUT</h2>
+      <div className='about-flex'>
+        <div>
+          <div className='headshot' alt='headshot'></div>
+        </div>
+        <div>
+          <p className='about-p'>
+            <em>You got games on your phone?</em> I do. That's how I got here, sort of. I love puzzles and problem solving, so web development is a constructive outlet for me to get some of that energy out, in a creative and lucrative way. Having worked in blue collar industries for the past decade, I am now ready to further destroy my posture working for you. I am currently undergoing a full stack (MERN specific) program through Vanderbilt University. Feel free to reach out to me via the contact section. Let's acquire this currency!
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About;
