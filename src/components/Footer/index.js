1f97dedc46d52b42afe33941b9ad93ce6a23504f
Count Zero <joelecox1@gmail.com>
2021-05-10 16:29:25 -0500

    footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p className='name-date'>©2021 Joel Cox</p>
+      <ul>
+        <li>
+          <a href='https://github.com/joelecox1'>
+            <h2>Github</h2>
+          </a>
+        </li>
+        <li>
+          <a href='https://www.linkedin.com/in/joel-cox-202731200/'>
+            <h2>LinkedIn</h2>
+          </a>
+        </li>
+        <li>
+          <a href='https://www.instagram.com/tropic.optics/'>
+            <h2>Instagram</h2>
+          </a>
+        </li>
+      </ul>
+    </footer >
+  )
+
+};
+
+export default Footer;
