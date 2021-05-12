ff4fbac981bebbe5ba7e467e1eb86ef9e9d0a90f
Count Zero <joelecox1@gmail.com>
2021-05-12 17:37:01 -0500

    links in footer open new tab

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,17 +6,17 @@ function Footer() {
       <p className='name-date'>©2021 Joel Cox</p>
       <ul>
         <li>
-          <a href='https://github.com/joelecox1'>
+          <a href='https://github.com/joelecox1' target='_blank' rel="noreferrer">
             <h2>Github</h2>
           </a>
         </li>
         <li>
-          <a href='https://www.linkedin.com/in/joel-cox-202731200/'>
+          <a href='https://www.linkedin.com/in/joel-cox-202731200/' target='_blank' rel="noreferrer">
             <h2>LinkedIn</h2>
           </a>
         </li>
         <li>
-          <a href='https://www.instagram.com/tropic.optics/'>
+          <a href='https://www.instagram.com/tropic.optics/' target='_blank' rel="noreferrer">
             <h2>Instagram</h2>
           </a>
         </li>
